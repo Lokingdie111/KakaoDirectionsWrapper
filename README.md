87764098e13c184f3c69b040872a78d6d7b5c480
lokingdie111 <lokingdie111@naver.com>
2025-11-03 21:19:33 +0900

    readme update.

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 카카오 길찾기 API로 요청을 보내고 응답을 사용하기 편리하게 하기 위한 Wrapper.
 ## 설치 
 Xcode의 SPM을 통해 프로젝트에 패키지를 추가할수 있습니다. \
-Repo 주소. `https://github.com/Lokingdie111/KakaoDirectionsWrapper.git` 
+Repo 주소. `https://github.com/Lokingdie111/KakaoDirectionsWrapper.git`
 ### 카카오 길찾기 API 키
 이 Wrapper를 통해 길찾기 요청을 보내기 위해서는 카카오디벨로퍼스에서 API 키를 발급받아 사용해야합니다.
-- 카카오 디벨로퍼스 홈페이지: `https://developers.kakao.com`
+- 카카오 디벨로퍼스 홈페이지: [바로가기](https://developers.kakao.com)
 
 ## DirectionAPI
 카카오 길찾기 API로 요청을 보내어 응답결과를 받습니다.
@@ -72,3 +72,7 @@ Task {
 
 ### 다중 출발지, 단일 목적지 - requestMultiOrigin()
 여러 출발지에서 한 목적지로까지의 경로 정보를 요약 정보로 받아올수 있습니다. (추후 업데이트 예정)
+## DirectionResponse
+request()와 requestMultiWaypoints()는 이 구조체로 반환됩니다. \
+이 구조체는 API응답 결과를 담고있습니다. \
+각각의 프로퍼티들의 의미는 Xcode의 Quick-help 또는 카카오 길찾기 API 도큐먼트에서 확인할수 있습니다.
--- Tests/KakaoDirectionsWrapperTests/KakaoDirectionsWrapperTests.swift
+++ Tests/KakaoDirectionsWrapperTests/KakaoDirectionsWrapperTests.swift
@@ -1,6 +1,41 @@
 import Testing
+import Foundation
 @testable import KakaoDirectionsWrapper
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+
+
+@Test func request() async throws {
+    
+    let origin: Position = Position(name: "출발지", longitude: 126.97815420, latitude: 37.5668601514026) // 서울 시청
+    let destination: Position = Position(name: "목적지",longitude: 126.979864052, latitude: 37.5643917261) // 주변 호텔
+    let waypoint1 = Position(name: "경유지", longitude: 126.9764161, latitude: 37.568598)
+    let directionAPI = DirectionAPI(apiKey: "")
+    let result = try? await directionAPI.request(origin: origin, destination: destination, wayPoints: [waypoint1])
+    #expect(result != nil)
+}
+
+@Test func requestMultiWaypoints() async throws {
+    let origin: Position = Position(name: "서울 시청", longitude: 126.97815420, latitude: 37.5668601514026)
+    let destination: Position = Position(name: "웨스턴 조선 서울",longitude: 126.979864052, latitude: 37.5643917261)
+    
+    let waypoint1 = Position(name: "경유지1", longitude: 126.9764161, latitude: 37.568598)
+    let waypoint2 = Position(name: "경유지2", longitude: 126.98414660, latitude: 37.571798563)
+    let waypoint3 = Position(name: "경유지3", longitude: 126.9774161, latitude: 37.568598)
+    let waypoint4 = Position(name: "경유지4", longitude: 126.98414660, latitude: 37.571798563)
+    let waypoint5 = Position(name: "경유지5", longitude: 126.9784161, latitude: 37.568598)
+    let waypoint6 = Position(name: "경유지6", longitude: 126.98414660, latitude: 37.571798563)
+    
+    let directionAPI = DirectionAPI(apiKey: "")
+    
+    let result = try? await directionAPI.requestMultiWaypoints(origin: origin, destination: destination, wayPoints: [waypoint1, waypoint2, waypoint3, waypoint4, waypoint5, waypoint6])
+    #expect(result != nil)
+    
+}
+
+@Test func reqeustMultiOrigin() async throws {
+    
+}
+
+@Test func requestMultiDestination() async throws {
+    
 }
